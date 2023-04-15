ea686422c6cbd7a6331da18761f9406d32acd9ab
Ayomide Aladesulu <ayomidealadesulu@gmail.com>
2023-04-15 01:39:21 +0100

    add: product search

--- src/components/Home/content/HomeContent/HomeContent.jsx
+++ src/components/Home/content/HomeContent/HomeContent.jsx
@@ -1,9 +1,9 @@
 const HomeContent = () => {
   return (
     <div className="mt-12">
-      <div className="content-row grid grid-cols-2 gap-[200px] mb-24">
-        <div className="col-span-1">
-          <div className="w-4/5 mt-16">
+      <div className="content-row grid grid-cols-2 gap-[200px] mb-12 lg:mb-24">
+        <div className="col-span-2 lg:col-span-1">
+          <div className="w-full lg:w-4/5 mt-16">
             <h1 className="text-5xl font-bold mb-4">
               Lorem ipsum dolor sit amet consectetur
             </h1>
@@ -18,15 +18,15 @@ const HomeContent = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-1">
+        <div className="hidden lg:block lg:col-span-1">
           <div className="relative h-full">
             <div className="z-50 image-2 h-2/5 w-2/5 absolute top-0 right-0"></div>
             <div className="image-1 h-4/5 w-4/5 absolute bottom-0 left-0"></div>
           </div>
         </div>
       </div>
-      <div className="content-row grid grid-cols-7 gap-[50px] h-[500px] mb-48">
-        <div className="col-span-2">
+      <div className="content-row grid grid-cols-1 md:grid-cols-2 lg:grid-cols-7 gap-[50px] h-auto lg:h-[500px] mb-12 lg:mb-48">
+        <div className="col-span-1 lg:col-span-2">
           <div>
             <p className="font-bold">Header Text</p>
             <p className="text-primary">
@@ -39,11 +39,11 @@ const HomeContent = () => {
             </p>
           </div>
         </div>
-        <div className="col-span-3">
+        <div className="hidden lg:block lg:col-span-3">
           <div className="image-1 h-full w-full"></div>
         </div>
-        <div className="col-span-2">
-          <div className="h-full flex flex-col justify-end">
+        <div className="col-span-1 lg:col-span-2">
+          <div className="h-full flex flex-col justify-start lg:justify-end">
             <p className="font-bold">Header Text</p>
             <p className="text-primary">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam,
@@ -56,12 +56,12 @@ const HomeContent = () => {
           </div>
         </div>
       </div>
-      <div className="content-row grid grid-cols-7 gap-[50px] bg-gray pb-24">
-        <div className="col-span-3">
+      <div className="content-row grid grid-cols-1 lg:grid-cols-7 gap-[50px] bg-gray lg:pb-12 pb-24">
+        <div className="hidden lg:block lg:col-span-3">
           <div className="image-1 h-[500px] w-full -mt-24"></div>
         </div>
-        <div className="col-span-4">
-          <div className="flex h-full flex-col justify-center w-2/3">
+        <div className="col-span-1 pt-12 lg:pt-0 lg:col-span-4">
+          <div className="flex h-full flex-col justify-center w-full lg:w-2/3">
             <h1 className="text-5xl font-bold mb-4">
               Lorem ipsum dolor sit amet consectetur
             </h1>
